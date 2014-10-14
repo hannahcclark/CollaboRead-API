fa6e2d84ea013458203b6f0e644e0ec21ba0cbfc
adempsey <amd0331@gmail.com>
2014-10-14 01:26:33 -0400

    added lecturercases endpoint

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var validator = require('validator')
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -26,6 +27,37 @@ app.get('/lecturers', function(req, res) {
     db.collection("users", function(err, collection) {
         collection.find({"type":"lecturer"}).toArray(function(err, documents) {
             res.send(documents)
+        })
+    })
+})
+
+app.get('/lecturercases', function(req, res) {
+    var lecturerID = validator.escape(req.query.lecturerID)
+    db.collection("users", function(err, usersCollection) {
+        usersCollection.findOne({"id_num": lecturerID}, function(err, user) {
+            if (err || user["type"] != "lecturer") {
+                console.log(err)
+                res.status(404).end()
+                res.send()
+            } else {
+                db.collection("caseSets", function(err, caseSetsCollection) {
+                    if (err) {
+                        console.log(err)
+                        res.status(404).end()
+                        res.send()
+                    } else {
+                        caseSetsCollection.find({"caseSet_id": {$in:user["caseSets"]} }).toArray(function(err, caseSet) {
+                            if (err) {
+                                console.log(err)
+                                res.status(404).end()
+                                res.send()
+                            } else {
+                                res.send(caseSet)
+                            }
+                        })
+                    }
+                })
+            }
         })
     })
 })
