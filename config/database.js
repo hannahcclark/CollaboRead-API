88e955dd4d2000fc2cf2ba77ed73f50e6e350413
adempsey <amd0331@gmail.com>
2014-10-29 15:29:23 -0400

    removed local test db address

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url' : /*process.env.MONGOLAB_URI ||*/ "mongodb://heroku_app30566011:f1rqkos5pk0jh6v5fbogfhqn9t@ds049180.mongolab.com:49180/collaboread_dev_2"
+    'url' : process.env.MONGOLAB_URI
 };
