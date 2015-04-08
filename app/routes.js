89091607af936be0aa23de98dcd014e7e1970479
adempsey <amd0331@gmail.com>
2015-04-08 19:20:51 -0400

    user colors are generated during registration

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,17 @@ var async = require('async');
 var bodyParser = require('body-parser');
 var bodyParserURLEncoded = bodyParser.urlencoded({extended: true});
 
+var fs = require('fs');
+
 var validator = require('validator');
 var bcrypt = require('bcrypt');
 var crypto = require('crypto');
+
+var colorGen = require('color-assignment-generator');
+
+fs.readFile(__dirname + "/colorFile/color.txt", function(err, data) {
+    colorGen.setSavedValues(JSON.parse(data));
+});
 
 var nodemailer = require('nodemailer');
 var markdown = require('nodemailer-markdown').markdown;
@@ -136,6 +144,7 @@ module.exports = function(http, ws) {
                                 "email": req.body.email,
                                 "password": bcrypt.hashSync(req.body.password,10),
                                 "caseSets": [],
+                                "color": colorGen.getColor(),
                                 "verified": false,
                                 "resetPasswordToken": token,
                                 "resetPasswordExpires": Date.now() + 3600000 //1 hour
@@ -145,6 +154,13 @@ module.exports = function(http, ws) {
                                 if (err) {
                                     res.status(404).end();
                                 } else {
+
+                                    fs.writeFile(__dirname + "/colorFile/color.txt", JSON.stringify(colorGen.getSavedValues()), function(err) {
+                                        if (err) {
+                                            return console.log(err);
+                                        }
+                                    });
+
                                     done(err, token, user);
                                 }
                             });
